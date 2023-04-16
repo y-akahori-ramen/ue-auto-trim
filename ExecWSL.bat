4a43769e7ec2bfd2aedb2a2484f3adb471f171e2
y-akahori-ramen <y.akahori.ramen@gmail.com>
2023-04-16 23:47:06 +0900

    update

--- ExecWSL.bat
+++ ExecWSL.bat
@@ -1,4 +1,4 @@
 
 wsl sudo service docker start
-wsl docker run --rm -it --gpus=all -v /mnt/c/MyPrograms/ue-auto-trim/data:/usr/workdata:rw  ue-auto-trim:1.0 --video /usr/workdata/video3.mp4 --dist /usr/workdata --prefix from_docker
+wsl docker run --rm -it  --gpus=all -v /mnt/c/MyPrograms/ue-auto-trim/sampledata:/usr/workdata:rw  ue-auto-trim:1.0 --video /usr/workdata/sample.mp4 --dist /usr/workdata --prefix from_docker
 wsl --shutdown
